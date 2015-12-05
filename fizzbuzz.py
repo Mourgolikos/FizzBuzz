3795077faa3057a05e2084ab8c3293ff0c12baeb
Triantafyllos Paschaleris <pashaler@hotmail.com>
2015-12-05 02:26:40 +0200

    problem extra requirement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,7 +8,7 @@ FizzBuzz Challenge Description:
     For multiples of 3, print "Fizz" instead of the number
     For multiples of 5, print "Buzz" instead of the number
     For multiples of 3 and 5, print "FizzBuzz" instead of the number
-    Here: fill an array instead of printing + some other complications
+    Extra: Print a new line after each case
 '''
 ##########
 #First Try. Keeping it simple and readable
